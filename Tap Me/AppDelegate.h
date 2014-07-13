dedd383cea798e1a2bace18f16546718a172680f
le-vai_l <ludovic.le-vaillant@epitech.eu>
2014-07-13 16:55:47 +0200

    Initial Commit

---
+++ Tap Me/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tap Me
+//
+//  Created by ludovic Le Vaillant on 13/07/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Tap Me/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Tap Me
+//
+//  Created by ludovic Le Vaillant on 13/07/2014.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
